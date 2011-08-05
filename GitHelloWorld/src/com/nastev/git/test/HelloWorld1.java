ddbcbab3d552379d1cabee1f0d35a39c6bc518a6
tasko.nastev <tasko.nastev@vodafone.com>
2011-08-05 14:36:46 +0200

    Den alten Schrott gelöscht

--- GitHelloWorld/src/com/nastev/git/test/HelloWorld1.java
+++ GitHelloWorld/src/com/nastev/git/test/HelloWorld1.java
@@ -7,13 +7,6 @@ public class HelloWorld1 {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt!");
-		System.out.println("sagt Tasko\n");
-		System.out.println("proposal1\n");
-		System.out.println("approval1\n");
-		System.out.println("proposal2\n");
-		System.out.println("approval2\n");
-		System.out.println("approval3\n");
-		System.out.println("master4\n");
 	}
 
 }

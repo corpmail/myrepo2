0511b19e0bf55db9171c375f78e7c087c6e2793b
tasko.nastev <tasko.nastev@vodafone.com>
2011-08-05 14:51:19 +0200

    M2

--- GitHelloWorld/src/com/nastev/git/test/HelloWorld1.java
+++ GitHelloWorld/src/com/nastev/git/test/HelloWorld1.java
@@ -7,7 +7,7 @@ public class HelloWorld1 {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt!");
-		System.out.println("Hallo Welt2");
+		System.out.println("Hallo Welt_Master");
 	}
 
 }

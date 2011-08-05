4c6ce8e5ec0895b86c16ea4eebf9d03080f2c8ca
tasko.nastev <tasko.nastev@vodafone.com>
2011-08-05 14:33:23 +0200

    Initial

---
+++ GitHelloWorld/src/com/nastev/git/test/HelloWorld1.java
@@ -0,0 +1,20 @@
+package com.nastev.git.test;
+
+public class HelloWorld1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hallo Welt!");
+		System.out.println("sagt Tasko\n");
+		System.out.println("proposal1\n");
+		System.out.println("approval1\n");
+		System.out.println("proposal2\n");
+		System.out.println("approval2\n");
+		System.out.println("approval3\n");
+		System.out.println("master4\n");
+	}
+
+}
+

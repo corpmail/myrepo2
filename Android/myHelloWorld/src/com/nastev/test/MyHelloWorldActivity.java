6affe0f6b8309bc09a9137f7e5a439bf2319a4fc
tasko <bla@bla.ch>
2012-05-03 10:27:16 +0200

    Initial!

---
+++ Android/myHelloWorld/src/com/nastev/test/MyHelloWorldActivity.java
@@ -0,0 +1,84 @@
+package com.nastev.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MyHelloWorldActivity extends Activity {
+	/** Called when the activity is first created. */
+	Button b1;
+	Button b2;
+	EditText et1;
+	final Context context = this;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		et1 = (EditText) findViewById(R.id.editText1);
+
+		b1 = (Button) findViewById(R.id.button1);
+		b1.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Date zeitstempel = new Date();
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+						"dd.MM.yyyy HH:mm:ss");
+				String dat = simpleDateFormat.format(zeitstempel);
+				System.out.println("Datum: "
+						+ simpleDateFormat.format(zeitstempel));
+				et1.setText(dat);
+			}
+		});
+
+		b2 = (Button) findViewById(R.id.button2);
+		b2.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+						context);
+
+				// set title
+				alertDialogBuilder.setTitle("Your Title");
+
+				// set dialog message
+				alertDialogBuilder
+						.setMessage("Click yes to exit!")
+						.setCancelable(false)
+						.setPositiveButton("Yes",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int id) {
+										// if this button is clicked, close
+										// current activity
+										MyHelloWorldActivity.this.finish();
+									}
+								})
+						.setNegativeButton("No",
+								new DialogInterface.OnClickListener() {
+									public void onClick(DialogInterface dialog,
+											int id) {
+										// if this button is clicked, just close
+										// the dialog box and do nothing
+										dialog.cancel();
+									}
+								});
+
+				// create alert dialog
+				AlertDialog alertDialog = alertDialogBuilder.create();
+
+				// show it
+				alertDialog.show();
+			}
+		});
+	}
+}

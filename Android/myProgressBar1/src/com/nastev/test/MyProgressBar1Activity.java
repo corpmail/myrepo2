4060c5bb71b6da3ef66d78dacee06fc4da048506
tasko.nastev <tasko.nastev@E0117743.internal.vodafone.com>
2012-05-03 10:40:33 +0200

    V1

---
+++ Android/myProgressBar1/src/com/nastev/test/MyProgressBar1Activity.java
@@ -0,0 +1,14 @@
+package com.nastev.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyProgressBar1Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        //Hier passiert was
+    }
+}
